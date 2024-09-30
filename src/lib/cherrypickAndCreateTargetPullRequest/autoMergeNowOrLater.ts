3b2dc99cdf77d291ab76db9ec552d0efaedff431
Leszek Kubik <39905449+intxgo@users.noreply.github.com>
2024-09-30 16:49:50 +0200

    merge immediately instead of toggling auto-merge button

--- src/lib/cherrypickAndCreateTargetPullRequest/autoMergeNowOrLater.ts
+++ src/lib/cherrypickAndCreateTargetPullRequest/autoMergeNowOrLater.ts
@@ -1,10 +1,5 @@
 import { ValidConfigOptions } from '../../options/options';
 import { mergePullRequest } from '../github/v3/mergePullRequest';
-import { GithubV4Exception } from '../github/v4/apiRequestV4';
-import {
-  enablePullRequestAutoMerge,
-  parseGithubError,
-} from '../github/v4/enablePullRequestAutoMerge';
 import { logger } from '../logger';
 import { ora } from '../ora';
 
@@ -23,29 +18,9 @@ export async function autoMergeNowOrLater(
   }
 
   try {
-    try {
-      await enablePullRequestAutoMerge(options, pullNumber);
-    } catch (e) {
-      if (!(e instanceof GithubV4Exception)) {
-        throw e;
-      }
-
-      logger.info(
-        `Auto merge: Failed to enable auto merge for PR "#${pullNumber}" due to ${e.message}`,
-      );
-
-      const { isMissingStatusChecks } = parseGithubError(e);
-      if (!isMissingStatusChecks) {
-        throw e;
-      }
-
-      // if auto merge cannot be enabled due to missing status checks, the PR should be merged immediately
-      logger.info('Auto merge: Attempting to merge immediately');
-      await mergePullRequest(options, pullNumber);
-      spinner.text = 'Auto-merge: Pull request was merged immediately';
-    }
-
-    spinner.succeed();
+    logger.info('Auto merge: Attempting to merge immediately');
+    await mergePullRequest(options, pullNumber);
+    spinner.text = 'Auto-merge: Pull request was merged immediately';
   } catch (e) {
     logger.warn(`Auto merge: An error occurred ${e}`);
     spinner.fail();
